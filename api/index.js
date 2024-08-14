2f067ed19befb5da349d84a55beeb33bfffe3450
Ruhit <rouhithmini1@gmail.com>
2024-08-14 12:57:54 +0600

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('DB is connected');
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app =express();
 
 
-app.listen(4000, () =>{
+app.listen(5000, () =>{
 
 
     console.log('Server run hoice');
