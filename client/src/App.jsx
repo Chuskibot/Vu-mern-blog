4e622297d84537eb5a9a6b8241eb0554eb8ff3fb
Ruhit <rouhithmini1@gmail.com>
2024-08-14 10:55:31 +0600

    creat pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SingUp from './pages/SingUp';
+import Home from './pages/Home';
+import SingIn from './pages/SingIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects'; 
+import About from './pages/About'; 
+
+
 export default function App() {
+
   return (
-    <h1 className="text-1xl text-red-500 ">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/sing-up' element={<SingUp />} />
+      <Route path='/sing-in' element={<SingIn />} />
+      <Route path='/projects' element={<Projects />} />
+
+      
+
+
+
+
+
+    </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
