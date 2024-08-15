22bd81a4d0b2c9895e64045ee7fbf6f011ca60d2
Ruhit <rouhithmini1@gmail.com>
2024-08-15 18:14:52 +0600

    creat sing in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const singup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,7 +14,7 @@ export const singup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +28,42 @@ export const singup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json({ message: "Singup successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const singin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    // eikhane Find user by email
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET
+      // dilam na Add expiration for security
+    );
+
+    const { password: pass, ...rest} = validUser._doc;
+
+    // Send token in cookie and respond with user info
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
